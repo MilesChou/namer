2801ee3282e971245f85e7aa2d263083f4c5452a
MilesChou <jangconan@gmail.com>
2017-12-31 22:48:27 +0800

    Refactoring before Day 20

--- command/generate.go
+++ command/generate.go
@@ -19,6 +19,10 @@ var (
 			},
 		},
 		Action: func(c *cli.Context) error {
+			t, _ := provider.ParseFile(c.GlobalString("provider"))
+
+			fmt.Println(t)
+
 			return generate(c)
 		},
 	}
--- provider/resource.go
+++ provider/resource.go
@@ -1,7 +1,31 @@
 package provider
 
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+)
+
+type NamesResource struct {
+	LastNames       []string `yaml:"lastNames"`
+	CharacterMale   []string `yaml:"characterMale"`
+	CharacterFemale []string `yaml:"characterFemale"`
+}
+
 var names = []string{
 	"金太郎",
 	"金城武",
 	"金智賢",
+}
+
+func ParseFile(file string) (res NamesResource, err error) {
+	r, err := ioutil.ReadFile(file)
+	if err != nil {
+		return res, err
+	}
+
+	if err := yaml.Unmarshal(r, &res); err != nil {
+		return res, err
+	}
+
+	return res, nil
 }
