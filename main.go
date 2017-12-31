cb229ae2ca26e42cad606709d40bdb357b0cbbfc
MilesChou <jangconan@gmail.com>
2017-12-31 21:00:24 +0800

    開始前先重構

--- main.go
+++ main.go
@@ -1,59 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
-	"github.com/MilesChou/namer/provider"
+	"github.com/MilesChou/namer/command"
 	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "Namer"
-	app.Commands = commands()
+	app.Commands = []cli.Command{
+		command.GenerateCommand,
+		command.StatusCommand,
+	}
 
 	app.Run(os.Args)
-}
-
-func commands() []cli.Command {
-	return []cli.Command{
-		{
-			Name:  "generate",
-			Usage: "產生假名",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "num",
-					Value: "10",
-					Usage: "產生數量",
-				},
-			},
-			Action: func(c *cli.Context) error {
-				num, err := strconv.Atoi(c.String("num"))
-
-				if err != nil {
-					return err
-				}
-
-				fmt.Println("Generate " + strconv.Itoa(num))
-
-				generator := provider.Create()
-
-				for i := 0; i < num; i++ {
-					fmt.Println(generator.Name())
-				}
-
-				return nil
-			},
-		},
-		{
-			Name:  "status",
-			Usage: "狀態",
-			Action: func(c *cli.Context) error {
-				fmt.Println("Hello Status")
-
-				return nil
-			},
-		},
-	}
 }
